c03068877cddd1c13c3341390bd0d7b37ad23f7b
Dandelion <dandelionxwitch@tutanota.com>
2024-08-07 21:54:45 -0600

    added script element to index.html; beginning to local time string script

---
+++ scripts/main.js
@@ -0,0 +1,3 @@
+let altareTime = document.querySelector(#altare);
+const altareEvent = new Date('August 5, 2024 0:00:00 GMT+00:00');
+altareTime.textContent = (altareEvent.toLocaleTimeString('en-US'));
